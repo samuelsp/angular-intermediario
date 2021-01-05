8f6cca4340ab55eb553c976b4e7610dbcfa4de0a
samuelsp <samp_81@yahoo.com.br>
2021-01-05 18:37:30 -0300

    [add] service de validação de campos

--- src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
+++ src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
@@ -1,5 +1,6 @@
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { ValidarCamposService } from 'src/app/shared/components/campos/validar-campos.service';
 
 @Component({
   selector: 'dio-cadastro-filmes',
@@ -11,7 +12,7 @@ export class CadastroFilmesComponent implements OnInit {
 
   cadastro: FormGroup;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(public validacao: ValidarCamposService, private fb: FormBuilder) {}
 
   get f() {
     return this.cadastro.controls;
--- src/app/shared/components/campos/validar-campos.service.ts
+++ src/app/shared/components/campos/validar-campos.service.ts
@@ -9,7 +9,7 @@ export class ValidarCamposService {
   constructor() { }
 
   hasErrorValidar(control: AbstractControl, errorName: string): boolean {
-    if ((control.dirty || control.touched) && this.hasError(control, errorName)) {
+    if((control.touched || control.dirty) && this.hasError(control, errorName)) {
       return true;
     }
     return false;
@@ -17,10 +17,5 @@ export class ValidarCamposService {
 
   hasError(control: AbstractControl, errorName: string): boolean {
     return control.hasError(errorName);
-  }
-
-  lengthValidar(control: AbstractControl, errorName: string): number {
-    const error = control.errors[errorName];
-    return error.requiredLength || error.min || error.max || 0;
   }
 }
