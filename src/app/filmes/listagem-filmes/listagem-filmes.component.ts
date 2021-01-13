be79ff97cb552c0d8b46d623f12aa4ad12abf47a
samuelsp <samp_81@yahoo.com.br>
2021-01-13 12:53:44 -0300

    [add] ordenacao e filtragem

--- src/app/filmes/listagem-filmes/listagem-filmes.component.ts
+++ src/app/filmes/listagem-filmes/listagem-filmes.component.ts
@@ -1,3 +1,4 @@
+import { FormGroup, FormBuilder } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 import { FilmesService } from 'src/app/core/filmes.service';
@@ -8,17 +9,51 @@ import { Filme } from 'src/app/shared/models/filme';
   templateUrl: './listagem-filmes.component.html',
   styleUrls: ['./listagem-filmes.component.scss']
 })
+
 export class ListagemFilmesComponent implements OnInit {
   readonly qtdPagina = 4;
   pagina = 0;
   filmes: Filme[] = [];
+  filtrosListagem: FormGroup;
+  generos: Array<string>;
+  texto: string;
+  genero: string;  
   
   constructor(
-              private filmesService: FilmesService) { }
+              private filmesService: FilmesService,
+              private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.filtrosListagem = this.fb.group({
+      texto: [''],
+      genero: ['']
+    });
+    
+    this.filtrosListagem.get('texto').valueChanges.subscribe((value: string) => {
+      this.texto = value;
+      this.resetarConsulta();
+
+    });
+    
+    this.filtrosListagem.get('genero').valueChanges.subscribe((value: string) => {
+      this.genero = value;
+      this.resetarConsulta();
+
+    });
+
+    this.generos = [
+      'Ação',
+      'Aventura',
+      'Comédia',
+      'Drama',
+      'Ficção Cientifica',
+      'Romance',
+      'Terror'
+    ];
+
     this.listarFilmes();
-  }
+
+  };
   
   onScroll(): void {
     this.listarFilmes();
@@ -26,8 +61,14 @@ export class ListagemFilmesComponent implements OnInit {
   
   private listarFilmes(): void {
     this.pagina++;
-    this.filmesService.listar(this.pagina, this.qtdPagina).subscribe(
+    this.filmesService.listar(this.pagina, this.qtdPagina, this.texto, this.genero).subscribe(
       (filmes: Filme[]) => this.filmes.push(...filmes));
+  }
+
+  private resetarConsulta(): void {
+    this.pagina = 0;
+    this.filmes = [];
+    this.listarFilmes();
   }
 
 }
